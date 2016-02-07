a0a5df14f6a2a9cf9720cc00a6b700fc1ceeee7d
roughterr <roughterr@gmail.com>
2016-02-07 17:19:00 +0200

    added the second query for the 4th lab

--- db/var18lab4.sql
+++ db/var18lab4.sql
@@ -16,4 +16,26 @@ AND "Hiredate" BETWEEN '01-01-2000' AND '31-12-2002';
 -- - строковая константа 'Инфо о фак-те компьютерных наук:' - Что выводится?
 -- - количество таких преподавателей    - Кол-во преподавателей
 -- - Количество читаемых ими дисциплин   -  Кол-во дисциплин
--- - КОЛИЧЕСТВО ГРУПП, В КОТОРЫХ ОНИ ПРОВОДЯТ ЗАНЯТИЯ - КОЛ-ВО ГРУПП
+-- - Количество групп, в которых они проводят занятия - Кол-во групп
+SELECT
+  'Инфо о фак-те компьютерных наук:' AS "Что выводится?",
+  COUNT(DISTINCT TEACHER."TchNo")    AS "Кол-во преподавателей",
+  COUNT(DISTINCT SUBJECT."SbjNo")    AS "Кол-во дисциплин",
+  COUNT(DISTINCT LECTURE."GrpNo")    AS "Кол-во групп"
+FROM
+  TEACHER
+JOIN DEPARTMENT
+ON
+  DEPARTMENT."DepNo" = TEACHER."DepNo"
+JOIN FACULTY
+ON
+  FACULTY."FacNo" = DEPARTMENT."FacNo"
+JOIN LECTURE
+ON
+  LECTURE."TchNo" = TEACHER."TchNo"
+JOIN SUBJECT
+ON
+  SUBJECT."SbjNo" = LECTURE."SbjNo"
+WHERE
+  FACULTY."Name"    = 'Chemistry'
+AND TEACHER."Post" IN ('counselor', 'professor', 'assistant professor');
