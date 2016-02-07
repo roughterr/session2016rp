2b7c6457c31d362c85d066eecd353cb4ebcd1f0e
roughterr <roughterr@gmail.com>
2016-02-07 23:55:31 +0200

    completed 5 of 7 scripts for 4th lab

--- db/lab2.sql
+++ db/lab2.sql
@@ -8,7 +8,8 @@ INSERT INTO FACULTY ("FacNo", "Name", "Building", "Fund") VALUES (5, 'Math',    
 INSERT INTO FACULTY ("FacNo", "Name", "Building", "Fund") VALUES (6, 'Management', '8', 300);
 INSERT INTO FACULTY ("FacNo", "Name", "Building", "Fund") VALUES (7, 'SoftwareEn', '17', 700);
 INSERT INTO FACULTY ("FacNo", "Name", "Building", "Fund") VALUES (8, 'Biology',    '1', 100);
-INSERT INTO FACULTY ("FacNo", "Name", "Building", "Fund") VALUES (9, 'Chemistry',  '2', 200);
+INSERT INTO FACULTY ("FacNo", "Name", "Building", "Fund", "Dean") 
+VALUES (9, 'Chemistry',  '2', 200, 'Sergiy Ivanov');
 INSERT INTO FACULTY ("FacNo", "Name", "Building", "Fund") VALUES (10, 'Geography', '9', 400);
 --Cathedra
 INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (1, 7, 'Department of Computer Science', 'Ivo Bobul', 17, 50);
@@ -17,42 +18,44 @@ INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VA
 INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (4, 9, 'Chemistry', 'Yana Nyzhegorodova', 2, 30);
 INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (5, 9, 'Materials Science and Metallurgy', 'Ksyusha Perevertun', 2, 10);
 INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (6, 9, 'Physics', 'Daniel Molina', 2, 10);
-INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (7, 9, 'Physics', 'Dmitriy Kostash', 2, 10);
-INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (8, 8, 'Biochemistry', 'Asya Subbotina', 1, 10);
+INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (7, 9, 'Physics', 'Dmitriy Kostash', 1, 10);
+INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (8, 8, 'Biochemistry', 'Asya Subbotina', NULL, 10);
 INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (9, 8, 'Pathology', 'Maria Kutsman', 1, 10);
 INSERT INTO DEPARTMENT ("DepNo", "FacNo", "Name", "Head", "Building", "Fund") VALUES (10, 5, 'Applied Mathematics and Theoretical Physics', 'Evgen Kashyn', 7, 30);
 --TEACHER table
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary", "Commission") 
   VALUES (1,  3, 'Andrey Radionov', 'counselor', 1234567, '02-01-1993', 9000, 500);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary", "Commission") 
-  VALUES (2,  3, 'Bogdan Deykalo', 'professor', 1234568, '02-01-1993', 3000, 100);
+  VALUES (2,  3, 'Bogdan Deykalo', 'professor', 7777777, '02-01-1993', 3000, 100);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary")
-  VALUES (3,  6, 'Lida Mozgovaya', 'assistant professor', 1234569, '02-01-1991', 2000);
+  VALUES (3,  6, 'Lida Mozgovaya', 'assistant professor', 7777777, '02-01-1991', 2000);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary")
   VALUES (4,  9, 'Ira Navrotskaya', 'assistant', 1234570, '02-01-1991', 2000);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary")
-  VALUES (5,  2, 'Ekaterina Shalamay', 'assistant professor', 1234571, '02-01-1991', 2000);
+  VALUES (5,  2, 'Ekaterina Shalamay', 'assistant professor', 7777777, '02-01-1991', 2000);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary", "Commission")
-  VALUES (6,  3, 'Natalia Kriminskaya', 'professor', 1234572, '07-02-2001', 2000, 150);
+  VALUES (6,  3, 'Natalia Kriminskaya', 'professor', 7777777, '07-02-2001', 2000, 150);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary")
   VALUES (7,  3, 'Nataliya Gayduk', 'assistant', 1234573, '02-01-1991', 2000);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary", "Commission")
-  VALUES (8, 3, 'Katya Yudenko', 'professor', 1234574, '04.04.2000', 2500, 400);
+  VALUES (8, 3, 'Katya Yudenko', 'professor', 7777777, '04.04.2000', 2500, 400);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary", "Commission")
   VALUES (9, 3, 'Yulia Ryabokon', 'assistant', 1234575,'02-01-1991', 900, 400);
 INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary")
-  VALUES (10, 3, 'Sergiy Ivanov', 'assistant', 1234576, '02-01-1991', 2000);
+  VALUES (10, 3, 'Sergiy Ivanov', 'professor', 1234576, '02-01-1991', 2000);
+INSERT INTO TEACHER ("TchNo", "DepNo", "Name", "Post", "Tel", "Hiredate", "Salary")
+  VALUES (11, 8, 'Anna Udina', 'assistant', 1122247, '07-02-2016', 2500);
 --SGROUP table
 INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (1,  1, 2, 123, 30, 1,  8);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (2,  3, 2, 123, 49, 2,  7);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (3,  6, 2, 123, 49, 3,  12);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (4,  9, 1, 123, 49, NULL,  12);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (5,  3, 2, 123, 49, 5,  2);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (6,  3, 1, 123, 49, 6,  12);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (7,  3, 1, 123, 49, 7,  12);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (8,  3, 2, 123, 49, 8,  9);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (9,  2, 1, 123, 49, 9,  5);
-INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (10, 9, 2, 123, 49, 10, 11);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (2,  3, 2, 124, 49, 2,  7);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (3,  6, 2, 125, 49, 3,  12);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (4,  9, 1, 126, 49, NULL,  12);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (5,  3, 2, 127, 49, 5,  2);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (6,  3, 1, 128, 49, 6,  12);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (7,  3, 1, 129, 49, 7,  12);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (8,  3, 2, 130, 49, 8,  9);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (9,  2, 1, 131, 49, 9,  5);
+INSERT INTO SGROUP ("GrpNo", "DepNo", "Course", "Num", "Quantity", "Curator", "Rating") VALUES (10, 9, 2, 132, 49, 10, 11);
 --SUBJECT table
 INSERT INTO SUBJECT ("SbjNo", "Name") VALUES (1, 'Operating Systems');
 INSERT INTO SUBJECT ("SbjNo", "Name") VALUES (2, 'Databases');
@@ -81,7 +84,7 @@ INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", 
 INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (NULL, 1,    1, 1, 'lection', 'Wed', 1, 3);
 INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (2, NULL, 1, 1, 'lection', 'Mon', 1, 4);
 INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (8, 1,    1, 2, 'practice','Mon', 1, 5);
-INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (2, 1,    1, 4, 'lection', 'Tue', 1, 1);
+INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (2, 1,    6, 4, 'lection', 'Tue', 1, 1);
 INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (7, 1,    1, 4, 'lab',     'Wed', 1, 2);
 INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (2, 1,    1, 4, 'lab',     'Tue', 1, 3);
 INSERT INTO LECTURE ("TchNo", "GrpNo", "SbjNo", "RomNo", "Type", "Day", "Week", "Lesson") VALUES (5, 9,    1, 4, 'lab',     'Wed', 1, 4);
--- db/var18lab4.sql
+++ db/var18lab4.sql
@@ -3,8 +3,8 @@
 -- 01.01.2000-31.12.2002. Столбцы результирующей таблицы должны иметь имена:
 -- «МИН зарплата» и «МАКС зарплата»
 SELECT
-  MIN("Salary" + "Commission") AS "МИН зарплата",
-  MAX("Salary" + "Commission") AS "МАКС зарплата"
+  MIN("Salary" + "Commission") AS "МИН зарплата"
+, MAX("Salary" + "Commission") AS "МАКС зарплата"
 FROM
   TEACHER
 WHERE
@@ -18,10 +18,10 @@ AND "Hiredate" BETWEEN '01-01-2000' AND '31-12-2002';
 -- - Количество читаемых ими дисциплин   -  Кол-во дисциплин
 -- - Количество групп, в которых они проводят занятия - Кол-во групп
 SELECT
-  'Инфо о фак-те компьютерных наук:' AS "Что выводится?",
-  COUNT(DISTINCT TEACHER."TchNo")    AS "Кол-во преподавателей",
-  COUNT(DISTINCT SUBJECT."SbjNo")    AS "Кол-во дисциплин",
-  COUNT(DISTINCT LECTURE."GrpNo")    AS "Кол-во групп"
+  'Инфо о фак-те компьютерных наук:' AS "Что выводится?"
+, COUNT(DISTINCT TEACHER."TchNo")    AS "Кол-во преподавателей"
+, COUNT(DISTINCT SUBJECT."SbjNo")    AS "Кол-во дисциплин"
+, COUNT(DISTINCT LECTURE."GrpNo")    AS "Кол-во групп"
 FROM
   TEACHER
 JOIN DEPARTMENT
@@ -39,3 +39,131 @@ ON
 WHERE
   FACULTY."Name"    = 'Chemistry'
 AND TEACHER."Post" IN ('counselor', 'professor', 'assistant professor');
+-- 3. Вывести перечень должностей, которые имеют преподаватели факультета,
+-- деканом которого является Иванов
+SELECT DISTINCT
+  TEACHER."Post"
+FROM
+  TEACHER
+JOIN DEPARTMENT
+ON
+  DEPARTMENT."DepNo" = TEACHER."DepNo"
+JOIN FACULTY
+ON
+  FACULTY."FacNo"= DEPARTMENT."FacNo"
+WHERE
+  FACULTY."Dean" LIKE '%Ivanov%';
+-- 4. Для каждой пары возможных значений преподаватель-группа вывести:
+-- - имя преподавателя
+-- - номер группы
+-- - количество лекций, читаемых этим преподавателем в этой группе
+-- - количество предметов, которые читает этот преподаватель этой группе
+SELECT DISTINCT
+  TEACHER."Name" AS "Имя преподавателя"
+, SGROUP."Num"   AS "Номер группы"
+, (
+    SELECT
+      COUNT(*)
+    FROM
+      LECTURE
+    WHERE
+      LECTURE."TchNo"   = TEACHER."TchNo"
+    AND LECTURE."GrpNo" = L."GrpNo"
+  ) AS "Number of lectures"
+, (
+    SELECT
+      COUNT(DISTINCT "SbjNo")
+    FROM
+      LECTURE
+    WHERE
+      LECTURE."TchNo"   = TEACHER."TchNo"
+    AND LECTURE."GrpNo" = L."GrpNo"
+  ) AS "Number of subjects"
+FROM
+  TEACHER
+JOIN LECTURE L
+ON
+  L."TchNo" = TEACHER."TchNo"
+JOIN SGROUP
+ON
+  SGROUP."GrpNo" = L."GrpNo"
+ORDER BY
+  TEACHER."Name";
+-- 5. По каждому корпусу  вывести следующую информацию в соответствующих
+-- столбцах:
+-- - номер корпуса. Если корпус NULL, то вывести константу “Нет корпуса” -
+-- Корпус
+-- - к-во кафедр в корпусе             - Кол-во кафедр
+-- - к-во факультетов в корпусе        - Кол-во факультетов
+-- - к-во преподавателей в корпусе     - Кол-во преподавателей
+-- - к-во номеров телефонов в корпусе  - Кол-во телефонов
+SELECT
+  BUILDING AS "Корпус"
+, (
+    SELECT
+      COUNT(*)
+    FROM
+      DEPARTMENT
+    WHERE
+      DEPARTMENT."Building" = BUILDING
+    OR
+      (
+        BUILDING                IS NULL
+      AND DEPARTMENT."Building" IS NULL
+      )
+  ) AS "Кол-во кафедр"
+,(
+    SELECT
+      COUNT(DISTINCT FACULTY."FacNo")
+    FROM
+      FACULTY
+    JOIN DEPARTMENT
+    ON
+      DEPARTMENT."FacNo" = FACULTY."FacNo"
+    WHERE
+      DEPARTMENT."Building" = BUILDING
+    OR
+      (
+        BUILDING                IS NULL
+      AND DEPARTMENT."Building" IS NULL
+      )
+  ) AS "Кол-во факультетов"
+, (
+    SELECT
+      COUNT(DISTINCT TEACHER."TchNo")
+    FROM
+      TEACHER
+    JOIN DEPARTMENT
+    ON
+      DEPARTMENT."DepNo" = TEACHER."DepNo"
+    WHERE
+      DEPARTMENT."Building" = BUILDING
+    OR
+      (
+        BUILDING                IS NULL
+      AND DEPARTMENT."Building" IS NULL
+      )
+  ) AS "Кол-во преподавателей"
+, (
+    SELECT
+      COUNT(DISTINCT TEACHER."Tel")
+    FROM
+      TEACHER
+    JOIN DEPARTMENT
+    ON
+      DEPARTMENT."DepNo" = TEACHER."DepNo"
+    WHERE
+      DEPARTMENT."Building" = BUILDING
+    OR
+      (
+        BUILDING                IS NULL
+      AND DEPARTMENT."Building" IS NULL
+      )
+  ) AS "Кол-во телефонов"
+FROM
+  (
+    SELECT DISTINCT
+      DEPARTMENT."Building" AS BUILDING
+    FROM
+      DEPARTMENT
+  );
